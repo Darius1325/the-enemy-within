f1e632eb5fe492cb938e792740eb16bac53cd957
Darius1325 <55551055+Darius1325@users.noreply.github.com>
2024-09-14 17:44:31 +0200

    Update format-json.sh

--- format-json.sh
+++ format-json.sh
@@ -3,7 +3,7 @@
 DATA_FOLDER="./data"
 for file in ${DATA_FOLDER}/*.json
 do
-  sed -e 's/\\r\\n/\\n/g' -f ${file} -i
+  sed -e 's/\\r\\n/\\n/g' ${file} -i
   cat ${file} | json_pp >> ${file}
   echo $(cat ${file} | json_pp)
 done
