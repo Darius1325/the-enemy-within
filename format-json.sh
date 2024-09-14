e307a01a427c2096152b10831332377c548f54e4
Darius1325 <55551055+Darius1325@users.noreply.github.com>
2024-09-14 17:57:22 +0200

    Update format-json.sh

--- format-json.sh
+++ format-json.sh
@@ -3,9 +3,5 @@
 DATA_FOLDER="./data"
 for file in ${DATA_FOLDER}/*.json
 do
-  #cat ${file}
-  #sed -e 's/\\r\\n/\\n/g' ${file} -i
-  #cat ${file} | json_pp >> ${file}
-  #echo $(cat ${file} | json_pp)
-  python3 -m json.tool ${file}
+  python3 -m json.tool ${file} >> ${file}
 done
