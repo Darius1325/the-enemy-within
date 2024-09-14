d3e7eb3dbfc2856d1353aa2ccb2af9bc0c9895c2
Darius1325 <55551055+Darius1325@users.noreply.github.com>
2024-09-14 17:05:13 +0200

    Update format-json.sh

--- format-json.sh
+++ format-json.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-DATA_FOLDER="data"
-
-for file in ${DATA_FOLDER}/*.json; do
+DATA_FOLDER="./data"
+echo ${DATA_FOLDER}
+for file in ${DATA_FOLDER}/*.json
+do
+  echo ${file}
   cat ${file} | json_pp >> ${file}.test
 done
