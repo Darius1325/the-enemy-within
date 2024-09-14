11335a70533e34d66426a31cdf65b19964492deb
Darius1325 <55551055+Darius1325@users.noreply.github.com>
2024-09-14 17:49:05 +0200

    Update format-json.sh

--- format-json.sh
+++ format-json.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 DATA_FOLDER="./data"
-for file in ${DATA_FOLDER}/*.json
-do
-  cat ${file}
+cat ${DATA_FOLDER}/Actors.json
+#for file in ${DATA_FOLDER}/*.json
+#do
+#  cat ${file}
   #sed -e 's/\\r\\n/\\n/g' ${file} -i
-  cat ${file} | json_pp >> ${file}
-  echo $(cat ${file} | json_pp)
-done
+#  cat ${file} | json_pp >> ${file}
+#  echo $(cat ${file} | json_pp)
+#done
