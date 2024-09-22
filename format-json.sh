9123d764d8713029a200faac659bfbec68b8fb9c
Dejan Paris <51233787+7evy@users.noreply.github.com>
2024-09-22 18:01:05 +0200

    Fix GHA (#4)

--- format-json.sh
+++ format-json.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 DATA_FOLDER="./data"
-for file in ${DATA_FOLDER}/*.json
+for file in $DATA_FOLDER/*.json
 do
-  python3 -m json.tool ${file} >> ${file}
+  temp=$(python3 -m json.tool $file)
+  cat <<< $temp > $file
 done
