5c31dcd619e40d9d53d586f505cae0bcef8d24dd
Adam <adamriviere@gmail.com>
2025-02-16 21:51:32 +0100

    Adding armours and first misc items

---
+++ js/plugins/TEW_OtherItems.js
@@ -0,0 +1,370 @@
+TEW.OTHER_ITEMS = TEW.OTHER_ITEMS || {
+    AMULET: {
+        name: "Amulet",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/0/2",
+        enc: 0,
+        availability: "COMMON"
+    },
+    BOOTS: {
+        name: "Boots",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/5/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    CLOTHING: {
+        name: "Clothing",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/6/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    COAT: {
+        name: "Coat",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/18/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    COSTUME: {
+        name: "Costume",
+        group: "CLOTHES",
+        nb: 1,
+        price: "1/0/0",
+        enc: 1,
+        availability: "SCARCE"
+    },
+    COURTLY_GARB: {
+        name: "Courtly Garb",
+        group: "CLOTHES",
+        nb: 1,
+        price: "12/0/0",
+        enc: 1,
+        availability: "SCARCE"
+    },
+    FACE_POWDER: {
+        name: "Face Powder",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/10/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    GLOVES: {
+        name: "Gloves",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/4/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    HAT: {
+        name: "Hat",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/4/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    HOOD: {
+        name: "Hood",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/5/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    JEWELLERY: {
+        name: "Jewellery",
+        group: "CLOTHES",
+        nb: 1,
+        price: "8/0/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    PERFUME: {
+        name: "Perfume",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/10/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    PINS: {
+        name: "Pins",
+        group: "CLOTHES",
+        nb: 6,
+        price: "0/10/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    RELIGIOUS_SYMBOL: {
+        name: "Religious Symbol",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/6/8",
+        enc: 0,
+        availability: "COMMON"
+    },
+    ROBES: {
+        name: "Robes",
+        group: "CLOTHES",
+        nb: 1,
+        price: "2/0/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    SCEPTRE: {
+        name: "Sceptre",
+        group: "CLOTHES",
+        nb: 1,
+        price: "8/0/0",
+        enc: 1,
+        availability: "RARE"
+    },
+    SHOES: {
+        name: "Shoes",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/5/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    SIGNET_RING: {
+        name: "Signet Ring",
+        group: "CLOTHES",
+        nb: 1,
+        price: "5/0/0",
+        enc: 0,
+        availability: "RARE"
+    },
+    TATOO: {
+        name: "Tatoo",
+        group: "CLOTHES",
+        nb: 1,
+        price: "0/4/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    UNIFORM: {
+        name: "Uniform",
+        group: "CLOTHES",
+        nb: 1,
+        price: "1/2/0",
+        enc: 1,
+        availability: "SCARCE"
+    },
+    WALKING_CANE: {
+        name: "Walking Cane",
+        group: "CLOTHES",
+        nb: 1,
+        price: "3/0/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    ALE_PINT: {
+        name: "Ale, pint",
+        group: "FOOD",
+        nb: 1,
+        price: "0/0/3",
+        enc: 0,
+        availability: "COMMON"
+    },
+    ALE_KEG: {
+        name: "Ale, keg",
+        group: "FOOD",
+        nb: 1,
+        price: "0/3/0",
+        enc: 2,
+        availability: "COMMON"
+    },
+    BUGMANS_ALE_PINT: {
+        name: "Bugman's Ale, pint",
+        group: "FOOD",
+        nb: 1,
+        price: "0/0/9",
+        enc: 0,
+        availability: "EXOTIC"
+    },
+    FOOD_ONE_DAY: {
+        name: "Food, Groceries/day",
+        group: "FOOD",
+        nb: 1,
+        price: "0/0/10",
+        enc: 1,
+        availability: "COMMON"
+    },
+    MEAL_INN: {
+        name: "Meal, inn",
+        group: "FOOD",
+        nb: 1,
+        price: "0/1/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    RATIONS_ONE_DAY: {
+        name: "Rations, one day",
+        group: "FOOD",
+        nb: 1,
+        price: "0/2/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    ROOM_COMMON: {
+        name: "Common Room/night",
+        group: "FOOD",
+        nb: 1,
+        price: "0/0/10",
+        enc: 0,
+        availability: "COMMON"
+    },
+    ROOM_PRIVATE: {
+        name: "Private Room/night",
+        group: "FOOD",
+        nb: 1,
+        price: "0/10/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    SPIRITS_PINT: {
+        name: "Spirits, pint",
+        group: "FOOD",
+        nb: 1,
+        price: "0/2/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    STABLES_NIGHT: {
+        name: "Stables/night",
+        group: "FOOD",
+        nb: 1,
+        price: "0/0/10",
+        enc: 0,
+        availability: "COMMON"
+    },
+    WINE_BOTTLE: {
+        name: "Wine Bottle",
+        group: "FOOD",
+        nb: 1,
+        price: "0/0/10",
+        enc: 0,
+        availability: "COMMON"
+    },
+    WINE_GLASS: {
+        name: "Wine Glass",
+        group: "FOOD",
+        nb: 1,
+        price: "0/0/4",
+        enc: 0,
+        availability: "COMMON"
+    },
+    ABACUS: {
+        name: "Abacus",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/3/4",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    ANIMAL_TRAP: {
+        name: "Animal Trap",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/2/6",
+        enc: 1,
+        availability: "COMMON"
+    },
+    ANTITOXIN_KIT: {
+        name: "Antitoxin Kit",
+        group: "TOOLS",
+        nb: 1,
+        price: "3/0/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    BOAT_HOOK: {
+        name: "Abacus",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/5/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    BROOM: {
+        name: "Broom",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/0/10",
+        enc: 2,
+        availability: "COMMON"
+    },
+    BUCKET: {
+        name: "Bucket",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/2/6",
+        enc: 1,
+        availability: "COMMON"
+    },
+    CHISEL: {
+        name: "Chisel",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/4/2",
+        enc: 0,
+        availability: "COMMON"
+    },
+    COMB: {
+        name: "Comb",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/0/10",
+        enc: 0,
+        availability: "COMMON"
+    },
+    CROWBAR: {
+        name: "Crowbar",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/2/6",
+        enc: 1,
+        availability: "COMMON"
+    },
+    CRUTCH: {
+        name: "Crutch",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/3/0",
+        enc: 2,
+        availability: "COMMON"
+    },
+    DISGUISE_KIT: {
+        name: "Disguise Kit",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/6/6",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    EAR_PICK: {
+        name: "Ear Pick",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/2/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    FISH_HOOKS: {
+        name: "Fish Hooks",
+        group: "TOOLS",
+        nb: 12,
+        price: "0/1/0",
+        enc: 0,
+        availability: "COMMON"
+    }// suite page 203
+}
