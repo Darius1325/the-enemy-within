e4f9fc2672ec29098e38e34256105bdff2dc7ca7
Adam <adamriviere@gmail.com>
2025-02-17 19:22:16 +0100

    Adds more misc items

--- js/plugins/TEW_OtherItems.js
+++ js/plugins/TEW_OtherItems.js
@@ -111,13 +111,29 @@ TEW.OTHER_ITEMS = TEW.OTHER_ITEMS || {
         enc: 0,
         availability: "COMMON"
     },
-    ROBES: {
-        name: "Robes",
+    ROBES_PRACTICAL: {
+        name: "Practical Robes",
         group: "CLOTHES",
         nb: 1,
-        price: "2/0/0",
+        price: "1/0/0",
         enc: 1,
-        availability: "COMMON"
+        availability: "RARE"
+    },
+    ROBES_STANDARD: {
+        name: "Standard Robes",
+        group: "CLOTHES",
+        nb: 1,
+        price: "8/0/0",
+        enc: 2,
+        availability: "EXOTIC"
+    },
+    ROBES_ELABORATE: {
+        name: "Elaborate Robes",
+        group: "CLOTHES",
+        nb: 1,
+        price: "30/0/0",
+        enc: 4,
+        availability: "EXOTIC"
     },
     SCEPTRE: {
         name: "Sceptre",
@@ -366,5 +382,381 @@ TEW.OTHER_ITEMS = TEW.OTHER_ITEMS || {
         price: "0/1/0",
         enc: 0,
         availability: "COMMON"
-    }// suite page 203
+    },
+    FLOOR_BRUSH: {
+        name: "Floor Brush",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/1/6",
+        enc: 0,
+        availability: "COMMON"
+    },
+    GAVEL: {
+        name: "Gavel",
+        group: "TOOLS",
+        nb: 1,
+        price: "1/0/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    HAMMER: {
+        name: "Hammer",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/3/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    HAND_MIRROR: {
+        name: "Hand Mirror",
+        group: "TOOLS",
+        nb: 1,
+        price: "1/1/6",
+        enc: 0,
+        availability: "EXOTIC"
+    },
+    HOE: {
+        name: "Hoe",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/4/0",
+        enc: 2,
+        availability: "COMMON"
+    },
+    KEY: {
+        name: "Key",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/1/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    LOCK_PICK: {
+        name: "Lock Pick",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/15/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    MANACLES: {
+        name: "Manacles",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/18/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    MOP: {
+        name: "Mop",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/1/0",
+        enc: 2,
+        availability: "COMMON"
+    },
+    NAILS: {
+        name: "Nails",
+        group: "TOOLS",
+        nb: 12,
+        price: "0/0/2",
+        enc: 0,
+        availability: "COMMON"
+    },
+    PAINT_BRUSH: {
+        name: "Paint Brush",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/4/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    PESTLE_AND_MORTAR: {
+        name: "Pestle and Mortar",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/14/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    PICK: {
+        name: "Pick",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/18/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    POLE: {
+        name: "Pole (3 yards)",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/8/0",
+        enc: 3,
+        availability: "COMMON"
+    },
+    QUILL_PEN: {
+        name: "Quill Pen",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/3/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    RAKE: {
+        name: "Rake",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/4/6",
+        enc: 2,
+        availability: "COMMON"
+    },
+    READING_LENS: {
+        name: "Reading Lens",
+        group: "TOOLS",
+        nb: 1,
+        price: "3/0/0",
+        enc: 0,
+        availability: "RARE"
+    },
+    SAW: {
+        name: "Saw",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/6/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    SICKLE: {
+        name: "Sickle",
+        group: "TOOLS",
+        nb: 1,
+        price: "1/0/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    SPADE: {
+        name: "Spade",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/8/0",
+        enc: 2,
+        availability: "COMMON"
+    },
+    SPIKE: {
+        name: "Spike",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/1/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    STAMP: {
+        name: "Stamp, engraved",
+        group: "TOOLS",
+        nb: 1,
+        price: "5/0/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    TONGS: {
+        name: "Tongs, steel",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/16/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    TELESCOPE: {
+        name: "Telescope",
+        group: "TOOLS",
+        nb: 1,
+        price: "5/0/0",
+        enc: 0,
+        availability: "RARE"
+    },
+    TWEEZERS: {
+        name: "Tweezers",
+        group: "TOOLS",
+        nb: 1,
+        price: "0/1/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    WRITING_KIT: {
+        name: "Writing Kit",
+        group: "TOOLS",
+        nb: 1,
+        price: "2/0/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    APOTHECARY_BOOK: {
+        name: "Apothecary Book",
+        group: "BOOKS",
+        nb: 1,
+        price: "8/0/0",
+        enc: 1,
+        availability: "SCARCE"
+    },
+    ART_BOOK: {
+        name: "Art Book",
+        group: "BOOKS",
+        nb: 1,
+        price: "5/0/0",
+        enc: 1,
+        availability: "SCARCE"
+    },
+    ACRYPTOGRAPHY_BOOK: {
+        name: "Cryptography Book",
+        group: "BOOKS",
+        nb: 1,
+        price: "8/0/0",
+        enc: 1,
+        availability: "EXOTIC"
+    },
+    ENGINEER_BOOK: {
+        name: "Engineer Book",
+        group: "BOOKS",
+        nb: 1,
+        price: "3/0/0",
+        enc: 1,
+        availability: "SCARCE"
+    },
+    LAW_BOOK: {
+        name: "Law Book",
+        group: "BOOKS",
+        nb: 1,
+        price: "15/0/0",
+        enc: 1,
+        availability: "RARE"
+    },
+    MAGIC_GRIMOIRE: {
+        name: "Magic Grimoire",
+        group: "BOOKS",
+        nb: 1,
+        price: "20/0/0",
+        enc: 1,
+        availability: "EXOTIC"
+    },
+    MEDICINE_BOOK: {
+        name: "Medicine Book",
+        group: "BOOKS",
+        nb: 1,
+        price: "15/0/0",
+        enc: 1,
+        availability: "RARE"
+    },
+    RELIGION_BOOK: {
+        name: "Religion Book",
+        group: "BOOKS",
+        nb: 1,
+        price: "1/0/0",
+        enc: 1,
+        availability: "COMMON"
+    },
+    GUILD_LICENSE: {
+        name: "AGuild License",
+        group: "BOOKS",
+        nb: 1,
+        price: "0/0/0",
+        enc: 0,
+        availability: "EXOTIC"
+    },
+    LEAFLET: {
+        name: "Leaflet",
+        group: "BOOKS",
+        nb: 1,
+        price: "0/1/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    LEGAL_DOCUMENT: {
+        name: "Leagl Document",
+        group: "BOOKS",
+        nb: 1,
+        price: "0/3/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    MAP: {// TODO more maps ?
+        name: "Map",
+        group: "BOOKS",
+        nb: 1,
+        price: "3/0/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    PARCHMENT: {
+        name: "Parchment",
+        group: "BOOKS",
+        nb: 1,
+        price: "0/1/0",
+        enc: 0,
+        availability: "COMMON"
+    },
+    BLACK_LOTUS: {
+        name: "Black Lotus",
+        group: "DRUGS",
+        nb: 1,
+        price: "20/0/0",
+        enc: 0,
+        availability: "EXOTIC"
+    },
+    HEARTKILL: {
+        name: "Heartkill",
+        group: "DRUGS",
+        nb: 1,
+        price: "40/0/0",
+        enc: 0,
+        availability: "EXOTIC"
+    },
+    MAD_CAP_MUSHROOMS: {
+        name: "Mad Cap Mushrooms",
+        group: "DRUGS",
+        nb: 1,
+        price: "5/0/0",
+        enc: 0,
+        availability: "EXOTIC"
+    },
+    MANDRAKE_ROOT: {
+        name: "Mandrake Root",
+        group: "DRUGS",
+        nb: 1,
+        price: "1/0/0",
+        enc: 0,
+        availability: "RARE"
+    },
+    MOONFLOWER: {
+        name: "Moonflower",
+        group: "DRUGS",
+        nb: 1,
+        price: "5/0/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    RANALDS_DELIGHT: {
+        name: "Ranald's Delight",
+        group: "DRUGS",
+        nb: 1,
+        price: "0/18/0",
+        enc: 0,
+        availability: "SCARCE"
+    },
+    SPIT: {
+        name: "Spit",
+        group: "DRUGS",
+        nb: 1,
+        price: "1/5/0",
+        enc: 0,
+        availability: "RARE"
+    },
+    WEIRDROOT: {
+        name: "Weirdroot",
+        group: "DRUGS",
+        nb: 1,
+        price: "0/4/0",
+        enc: 0,
+        availability: "RARE"
+    }
 }
