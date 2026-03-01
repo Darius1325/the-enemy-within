a5191de2bf98780cd81cda077763bd6f58333438
Adam <adamriviere@gmail.com>
2026-03-01 21:22:56 +0100

    adding new characters descriptions and images

--- src/plugins/constants/TEW_CharacterDescriptions.ts
+++ src/plugins/constants/TEW_CharacterDescriptions.ts
@@ -23,5 +23,104 @@ TEW.DATABASE.CHARACTER_DESCRIPTIONS = [
                 content: "Most of Gustav's conversation revolves around ensuring his guests are well looked after, the state of the weather, the condition of the roads, and anything else that springs into his mind."
             }
         ]
+    },
+    {
+        id: 1,
+        title: "Herpin Stiggerwurt",
+        paragraphs : [
+            {
+                image: "herpin"
+            },
+            {
+                content: "Herpin has a thick Middenlander accent, greying mid-brown hair, and blue eyes. He is almost 30, but looks older. He’s tall, thin, and taciturn, and has no desire to make anyone feel welcome."
+            },
+            {
+                content: "He hails from the nearby hamlet of Moersum, and is employed for his brewing skills and work ethic, not his interpersonal skills."
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Gunnar and Hultz",
+        paragraphs : [
+            {
+                image: "gunnar"
+            },
+            {
+                content: "Sandy-haired Gunnar and dark-bearded Hultz are the coachmen for Ratchett Lines. Both men are of medium height; but Hultz, now in his 40s, is a good bit fatter."
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Lady Isolde von Strudeldorf",
+        paragraphs : [
+            {
+                image: "isolde"
+            },
+            {
+                content: "A typical Drakwalder noblewoman, Lady Isolde von Strudeldorf is willowy with red-golden hair, green eyes, and a refined Reiklander noble accent."
+            },
+            {
+                content: "She is young, headstrong, and covers her lack of confidence with an air of snobbishness. She dresses in the finest clothing and has a variety of unusual, elegant hats."
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Janna Elleiner",
+        paragraphs : [
+            {
+                image: "janna"
+            },
+            {
+                content: "Only 16, Janna is thin with fine, mousy hair and blue-grey eyes."
+            },
+            {
+                content: "She says very little, although sometimes Lady Isolde forces her to speak on her behalf. When this happens, she speaks with a soft Drakwalder accent, and looks deeply uncomfortable. For all her timidity, Janna is very good at her job."
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "Marie Schutz",
+        paragraphs : [
+            {
+                image: "marie"
+            },
+            {
+                content: "Like a Middenlander warrior-woman of legend, Marie is unusually tall, athletic, and muscular, and has thick, dark-blonde hair and ice-blue eyes."
+            },
+            {
+                content: "In her early 30s, she wears dark, sombre clothing, and never smiles. Marie has worked for the von Strudeldorf family for 15 years, and is more than capable of looking after her Lady Isolde."
+            }
+        ]
+    },
+    {
+        id: 6,
+        title: "Ernst Heidlemann",
+        paragraphs : [
+            {
+                image: "ernst"
+            },
+            {
+                content: "In his early 20s, Heidlemann is of average height and is trying hard to grow a beard. He has lank, brown hair and paleblue eyes."
+            }
+        ]
+    },
+    {
+        id: 7,
+        title: "Phillipe Descartes",
+        paragraphs : [
+            {
+                image: "phillipe"
+            },
+            {
+                content: "Phillipe is from Parravon in Bretonnia, and has been travelling the Empire for six months. Now in his 40s, he was previously a mercenary sergeant in the army of a local duke"
+            },
+            {
+                content: "He is tall, handsome, and wears a lace-trimmed shirt and embroidered jacket and breeches. He has long, dark curly hair and an artificial ‘beauty spot’ on his left cheek."
+            }
+        ]
     }
 ];
